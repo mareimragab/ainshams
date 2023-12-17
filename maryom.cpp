eb4e2e93f70df9aaf8d932d66b6df8a01a107806
mareimragab <105663579+mareimragab@users.noreply.github.com>
2023-12-17 02:15:15 +0200

    changing sum

---
+++ maryom.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a,int b);
+int main()
+{
+    cout << "Hello world!" << endl;
+      cout << "i world!" << endl;
+        cout << "can world!" << endl;
+    cout<< sum(2,4);
+    
+    return 0;
+}
+int sum(int a,int b){
+    return a+b;
+}
