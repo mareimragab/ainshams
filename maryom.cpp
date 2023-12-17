164d7145a3a521c3b23b05f7afc540b3ffffec0a
mareimragab <105663579+mareimragab@users.noreply.github.com>
2023-12-17 02:45:42 +0200

    delet g

--- maryom.cpp
+++ maryom.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-<<<<<<< HEAD
+
 using namespace std;
 
 int sum(int a,int b);
 int main()
 {
-    cout << "Hello world!" << endl;
-      cout << "i world!" << endl;
-        cout << "can world!" << endl;
+    
     cout<< sum(2,4);
     
     return 0;
@@ -15,13 +13,3 @@ int main()
 int sum(int a,int b){
     return a+b;
 }
-=======
-
-using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
->>>>>>> math-feature
