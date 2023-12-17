91e08e05aa5d0a258de84ebff7e02b64d74398d3
mareimragab <105663579+mareimragab@users.noreply.github.com>
2023-12-17 18:54:45 +0200

    New (#4)
    * change summation to take 3 integars
    
    * changing sum
    
    * delet g
    
    * rebase2
    
    * rebase
    
    * rebasel
    
    * bb
    
    * Create README.md
    
    hjkjk
    
    * new branch

---
+++ maryom.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int sum(int a,int b);
+int main()
+{
+
+    cout << "Hello world!" << endl;
+    cout << "i world!" << endl;
+    cout << "can world!" << endl;
+    cout << "Hello world!" << endl;   
+    cout << "my" << endl; 
+   cout<<"maryom";  
+    cout<< sum(2,4);
+    
+    return 0;
+}
+int sum(int a,int b){
+    return a+b;
+}
